bb3ccf4a3cab0369a4abe123694090985df03efd
Adam Dubiel <adam.dubiel@allegro.pl>
2013-10-29 15:35:40 +0100

    thin abstraction to allow getter serialization

---
+++ src/main/java/org/bitbucket/adubiel/jason/attribute/FieldAttribute.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2013 Adam Dubiel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bitbucket.adubiel.jason.attribute;
+
+import com.google.gson.internal.$Gson$Preconditions;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ *
+ * @author Adam Dubiel
+ */
+public class FieldAttribute implements Attribute {
+
+    private final Field field;
+
+    public FieldAttribute(Field f) {
+        $Gson$Preconditions.checkNotNull(f);
+        this.field = f;
+    }
+
+    public Class<?> getDeclaringClass() {
+        return field.getDeclaringClass();
+    }
+
+    public String getName() {
+        return field.getName();
+    }
+
+    public Type getDeclaredType() {
+        return field.getGenericType();
+    }
+
+    public Class<?> getDeclaredClass() {
+        return field.getType();
+    }
+
+    public <T extends Annotation> T getAnnotation(Class<T> annotation) {
+        return field.getAnnotation(annotation);
+    }
+
+    public Collection<Annotation> getAnnotations() {
+        return Arrays.asList(field.getAnnotations());
+    }
+
+    public boolean hasModifier(int modifier) {
+        return (field.getModifiers() & modifier) != 0;
+    }
+
+    public Object get(Object instance) throws IllegalAccessException {
+        return field.get(instance);
+    }
+
+    public boolean isSynthetic() {
+        return field.isSynthetic();
+    }
+}
---
+++ src/main/java/org/bitbucket/adubiel/jason/attribute/PropertyAttribute.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2013 Adam Dubiel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bitbucket.adubiel.jason.attribute;
+
+import java.beans.PropertyDescriptor;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ *
+ * @author Adam Dubiel
+ */
+public class PropertyAttribute implements Attribute {
+
+    private final PropertyDescriptor descriptor;
+
+    private final Method readMethod;
+
+    public PropertyAttribute(PropertyDescriptor descriptor) {
+        this.descriptor = descriptor;
+        this.readMethod = descriptor.getReadMethod();
+    }
+
+    public String getName() {
+        return descriptor.getName();
+    }
+
+    public Class<?> getDeclaringClass() {
+        return readMethod.getDeclaringClass();
+    }
+
+    public Type getDeclaredType() {
+        return readMethod.getGenericReturnType();
+    }
+
+    public Class<?> getDeclaredClass() {
+        return readMethod.getReturnType();
+    }
+
+    public <T extends Annotation> T getAnnotation(Class<T> annotation) {
+        return readMethod.getAnnotation(annotation);
+    }
+
+    public Collection<Annotation> getAnnotations() {
+        return Arrays.asList(readMethod.getDeclaredAnnotations());
+    }
+
+    public boolean hasModifier(int modifier) {
+        return (readMethod.getModifiers() & modifier) != 0;
+    }
+
+    public Object get(Object instance) throws IllegalAccessException {
+        try {
+            return readMethod.invoke(instance);
+        } catch (IllegalArgumentException argumentException) {
+            throw new IllegalAccessException();
+        } catch (InvocationTargetException invocationTargetException) {
+            throw new IllegalAccessException();
+        }
+    }
+
+    public boolean isSynthetic() {
+        return readMethod.isSynthetic();
+    }
+
+}
